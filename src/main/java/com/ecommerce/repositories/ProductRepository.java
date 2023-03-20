3fb6b7319fc5654c85e6a24232dfa709a7dd923b
Lightclaw55 <danakhnoukh@yahoo.com>
2023-03-20 11:50:57 -0400

    ProductDTO and Service

--- src/main/java/com/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/repositories/ProductRepository.java
@@ -3,10 +3,16 @@ package com.ecommerce.repositories;
 import com.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository {
 
     Product findProductById(int id);
 
     Product findProductByName(String name);
+
+    List<Product> findAllProduct();
+
+    Product save(Product p);
 
 }
--- src/main/java/com/ecommerce/service/ProductService.java
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -1,4 +1,38 @@
 package com.ecommerce.service;
 
+import com.ecommerce.model.Product;
+import com.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ProductService {
+
+    private final ProductRepository productRepo;
+
+    @Autowired
+    public ProductService(ProductRepository productRepo)
+    {
+        this.productRepo = productRepo;
+    }
+
+    public Product getProductById(int id)
+    {
+        return productRepo.findProductById(id);
+    }
+
+    public Product getProductByName(String name)
+    {
+        return productRepo.findProductByName(name);
+    }
+
+    public List<Product> getAllProducts()
+    {
+        return productRepo.findAllProduct();
+    }
+
+    public Product save(Product p)
+    {
+        return productRepo.save(p);
+    }
 }
